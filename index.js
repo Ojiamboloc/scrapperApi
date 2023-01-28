a97d21d729f39a73f2bbbe2a21c88f5d05afb0e0
Ojiamboloc <ojiamboloc@gmail.com>
2023-01-28 00:39:13 -0800

    get products

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const request = require("request-promise");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const apiKey = "4382a91d6bfce9be6910ff1a09168d30";
+const baseUrl = `http://api.scraperapi.com?api_keys=${apiKey}&autoparse=true`;
+app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("Welcome to Amazon Scrapper API");
+});
+//get producr details
+app.get("/products/:productID", async (req, res) => {
+  const { productID } = req.params;
+  try {
+    const response = await request(
+      `${baseUrl}&url=https://www.amazon.com/dp/&{productID}`
+    );
+    res.json(response);
+  } catch (error) {
+    res.json(error)}
+});
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
